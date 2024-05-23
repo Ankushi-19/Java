6a036835914f95512c4866fa10e5ad39160162d9
Ankushi-19 <ankushidutta2001@gmail.com>
2024-05-23 12:25:04 +0530

    first 10 numbers in Fibonacci series and their sum

---
+++ fibo.java
@@ -0,0 +1,33 @@
+//4.Write a java program to print first 10 numbers in Fibonacci series. Also print their sum.
+
+import java.io.*;
+import java.util.*;
+class fibonacci
+{
+  void fn(){
+   int a,b,c,s=0;
+   Scanner sc=new Scanner(System.in);
+  int n=10;
+   a=0;
+   b=1;
+   for(int i=1;i<=n;i++)
+     {
+       c=a+b;
+       a=b;
+       b=c;
+       System.out.println(c);
+       s=s+c;    
+     }
+   
+   System.out.println(s);
+}
+}
+
+class fibo{
+public static void main(String args[]){
+  fibonacci obj=new fibonacci();
+  obj.fn();
+
+}
+}
+   
